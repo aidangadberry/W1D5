96fe750e7d4a645553060103f5f15c3f6b2fc459
aidangadberry <agadberr@gmail.com>
2018-06-22 11:11:55 -0700

    Add parent assignment

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,18 @@
 class PolyTreeNode
+  attr_accessor :value, :parent, :children
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(node)
+    @parent.children.delete(self) unless @parent.nil?
+    @parent = node
+    node.children << self unless node.nil?
+  end
+
+
 
 end
